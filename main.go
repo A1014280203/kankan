7b0ecb6422393b31ce6565a0e2c173d9bde52fc9
spaceship-alpha <1014280203@qq.com>
2019-08-21 15:15:48 +0800

    data load finished

--- main.go
+++ main.go
@@ -1,40 +1,133 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/devfeel/dotweb"
+	"github.com/devfeel/middleware/cors"
+	_ "github.com/go-sql-driver/mysql"
+	"strconv"
+	"time"
 )
 
 type Card struct {
-	Title   string
-	Content string
-	DocURL  string
-	MpName  string
-	Avatar  string
+	Title      string
+	Content    string
+	DocURL     string
+	MpName     string
+	Avatar     string
+	CreateTime uint32
 }
+
+var db *sql.DB
 
 func main() {
 	app := dotweb.New()
-	app.SetDevelopmentMode()
+	//set debug mode
+	//app.SetDevelopmentMode()
+	option := cors.NewConfig().UseDefault()
+
+	db, _ = sql.Open("mysql", "root:AbLvx5gOcUw02BG@tcp(49.235.9.27:3306)/weread?charset=utf8")
+	defer db.Close()
+
 	app.HttpServer.ServerFile("/static/*filepath", "./static/")
 	app.HttpServer.Renderer().SetTemplatePath("./templates/")
-	app.HttpServer.GET("/", func(context dotweb.Context) error {
+	app.HttpServer.GET("/", IndexView)
+	app.HttpServer.GET("/posts", MorePost).Use(cors.Middleware(option))
+	app.HttpServer.GET("/h", func(context dotweb.Context) error {
 		return context.WriteString("Hello World")
 	})
-	app.HttpServer.GET("/posts", IndexView)
 	fmt.Println("Start on http://127.0.0.1:8080")
-	app.StartServer(8080)
+	err := app.StartServer(8080)
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 func IndexView(ctx dotweb.Context) error {
-	url := "http://wx.qlogo.cn/mmhead/Q3auHgzwzM5Bp39F4nxFK6e8yMJ6Ju5SneIotBH1RptRJ8VBq8IZ4Q/0"
-	c := Card{"标题", "内容", "链接", "微信公众号", url}
-	lst := []Card{c, c}
-	ctx.ViewData().Set("Cards", lst)
+	/* set cards and last timestamp to html */
+	nowTimeStamp := uint32(time.Now().Unix())
+	rows := dbc(nowTimeStamp)
+	defer rows.Close()
+	var cards []Card
+	var followTime uint32
+	for rows.Next() {
+		var card Card
+		err := rows.Scan(&card.Title, &card.Content, &card.DocURL, &card.MpName, &card.Avatar, &card.CreateTime)
+		if err != nil {
+			panic(err.Error())
+		}
+		followTime = card.CreateTime
+		cards = append(cards, card)
+	}
+
+	ctx.ViewData().Set("cards", cards)
+	ctx.ViewData().Set("followTime", followTime)
 	err := ctx.View("index.html")
 	return err
+}
+
+func MorePost(ctx dotweb.Context) error {
+	/* return cards and last timestamp as JSON to user */
+	timeString := ctx.QueryString("followTime")
+	followTime64, err := strconv.ParseUint(timeString, 10, 32)
+	followTime := uint32(followTime64)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rows := dbc(followTime)
+	defer rows.Close()
+	var cards []Card
+	for rows.Next() {
+		var card Card
+		err := rows.Scan(&card.Title, &card.Content, &card.DocURL, &card.MpName, &card.Avatar, &card.CreateTime)
+		if err != nil {
+			panic(err.Error())
+		}
+		followTime = card.CreateTime
+		cards = append(cards, card)
+	}
+
+	data := make(map[string]interface{})
+	data["cards"] = cards
+	data["followTime"] = followTime
+	b, err := json.Marshal(data)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = ctx.WriteJsonBlob(b)
+
+	return err
+}
+
+func dbc(followTime uint32) *sql.Rows {
+	aliasCol := "title as Title, content as Content, doc_url as DocURL, mp_name as MpName, avatar as Avatar, createTime as CreateTime"
+	stat, err := db.Prepare(fmt.Sprintf("SELECT %s FROM post WHERE createTime < ? ORDER BY createTime DESC limit 10", aliasCol))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stat.Close()
+
+	rows, err := stat.Query(followTime)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return rows
 }
 
 // todo: dbc
 // todo: improve <a> style
 // todo: posts API
+
+/*
+
+index init the end timestamp
+
+restAPI follow the timestamp index set
+
+re-request index will re-init the end timestamp
+
+*/
